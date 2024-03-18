824f4f584c28f9cb55715f75750f8e8cad52b4a8
Jozsef Gorcs <joeygeek@gmail.com>
2024-03-18 20:36:04 +0100

    Add some bullet improvements #10

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,12 @@ extends AnimatedSprite2D
 var velocity = Vector2(0, 0)
 const SPEED = Vector2(125, 90) 
 const BULLET = preload("res://player/player_bullet/player_bullet.tscn")
+const BULLET_OFFSET = 7
+var can_shoot = true
+
+@onready var reload_timer = $ReloadTimer
+
+
 func _process(delta):
 	velocity.x = Input.get_axis("move_left","move_right")
 	velocity.y = Input.get_axis("move_up","move_down")
@@ -14,11 +20,24 @@ func _process(delta):
 	elif velocity.x < 0:
 		flip_h = true
 		
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = BULLET.instantiate()
-		bullet_instance.global_position = global_position
+		
 		get_tree().current_scene.add_child(bullet_instance)  
+		if flip_h:
+			bullet_instance.velocity.x = -1
+			bullet_instance.velocity.y = -bullet_instance.velocity.y
+			bullet_instance.flip_h = true
+			bullet_instance.global_position = global_position - Vector2(BULLET_OFFSET,0)
+		else:
+			bullet_instance.global_position = global_position + Vector2(BULLET_OFFSET,0)	
+		can_shoot = false
+		reload_timer.start()
 	
 
 func _physics_process(delta):
 	global_position += velocity * delta * SPEED
+
+
+func _on_reload_timer_timeout():
+	can_shoot = true # Replace with function body.
--- player/player_bullet/player_bullet.gd
+++ player/player_bullet/player_bullet.gd
@@ -4,5 +4,13 @@ var velocity = Vector2(1,0)
 
 const SPEED = 300
 
+func _ready():
+	rotation_degrees = randf_range(-7,7)
+	velocity = velocity.rotated(rotation)
+
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free() # Replace with function body.
